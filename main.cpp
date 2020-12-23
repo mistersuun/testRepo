1b60dbbdaf6ba342eb9bf319726e8638c52f1c3a
Mr. Sun <futiliter@Futiliters-MBP.home>
2020-12-22 23:50:13 -0500

    New file

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  allo
+//
+//  Created by Futiliter on 2020-12-22.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    cout << "Hello world" << endl;
+    
+    return 0;
+}
